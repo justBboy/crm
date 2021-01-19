d4f4e9004fc29e38608e3e7088c65caefe899fe7
Bboy <bornscience1@gmail.com>
2021-01-18 17:57:50 -0800

    heroku setup

--- crm/settings.py
+++ crm/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -159,3 +160,6 @@ REST_FRAMEWORK = {
 REST_KNOX = {
     'TOKEN_TTL': timedelta(hours=24)
 }
+
+#django heroku
+django_heroku.settings(locals())
